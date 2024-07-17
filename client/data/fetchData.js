16f5dd7eb894b5db3ae23733073764d7ef4fdd5f
Husoschi Ionut-Catalin <ionuthusoschi@gmail.com>
2024-07-17 20:38:35 +0300

    console error

--- client/data/fetchData.js
+++ client/data/fetchData.js
@@ -6,12 +6,16 @@ export const fetchData = async (endpoint) => {
     const res = await fetch(`${process.env.BASE_URL_STRAPI}${endpoint}`, {
       method: "GET",
       headers: {
+        "Content-Type": "application/json",
         Authorization: `Bearer  ${process.env.TOKEN_STRAPI}`,
       },
     });
     const data = res.json();
     return data;
   } catch (error) {
-    console.log(error);
+    console.error(error);
+    throw new Error(
+      `Please check if your server is running and you set all the required tokens.`
+    );
   }
 };
