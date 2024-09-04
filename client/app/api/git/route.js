36926a8a0effab577f2e557b0974f99893cf0bd0
Husoschi Ionut-Catalin <ionuthusoschi@gmail.com>
2024-09-04 15:02:34 +0300

    cache no store

--- client/app/api/git/route.js
+++ client/app/api/git/route.js
@@ -1,10 +1,12 @@
 import { Octokit } from "octokit";
+import { revalidateTag, unstable_cache, unstable_noStore } from "next/cache";
 
 const octokit = new Octokit({
   auth: process.env.TOKEN_GIT_HUB,
 });
 
 export async function GET(request) {
+  unstable_noStore();
   try {
     const res = await octokit.request("GET /users/{username}/repos", {
       username: "ionut",
